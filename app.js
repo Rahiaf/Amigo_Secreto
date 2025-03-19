60f5f54b68c54abeda6971e56c88c307c2aab122
Rahiaf <luciano.f.calixto@gmail.com>
2025-03-18 22:45:43 -0300

    Projeto Final

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let inputAmigo = document.getElementById('amigo');
+  let nomeAmigo = inputAmigo.value.trim();
+
+  if (nomeAmigo === '') {
+    alert('Por favor, insira um nome.');
+    return;
+  }
+
+  amigos.push(nomeAmigo);
+
+  inputAmigo.value = '';
+
+    atualizarListaAmigos();
+}
+
+  function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+      let itemLista = document.createElement('li');
+      itemLista.textContent = amigos[i];
+        
+      listaAmigos.appendChild(itemLista);
+    }
+    }
+
+    function sortearAmigo() {
+      if (amigos.length === 0) {
+        alert('Adicione pelo menos um amigo para sortear.');
+        return;
+      }
+
+      let Aleatorio = Math.floor(Math.random() * amigos.length);
+
+      let amigoSorteado = amigos[Aleatorio];
+
+      let resultado = document.getElementById('resultado');
+      resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+
+    }
